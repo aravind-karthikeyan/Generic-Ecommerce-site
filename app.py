6df78463c0575ec68aaa277fbe9bbd0e6c8fe264
aravind_k <aravind@jungroo.com>
2020-04-28 10:58:08 +0530

    added logic for adding products to cart and removing products from cart

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from bson import ObjectId # For ObjectId to work
 from pymongo import MongoClient
 import os
 import bcrypt
+from collections import Counter
 
 app = Flask(__name__)
 title = "TODO sample application with Flask and MongoDB"
@@ -16,8 +17,7 @@ users = db.users
 @app.route('/')
 def index():
     if 'username' in session:
-        return render_template("products.html",products = products.find())
-
+        return render_template("products.html",products = products.find(), user = users.find_one({"username" : session["username"]}))
     return render_template('index.html')
 
 @app.route('/login', methods=['POST'])
@@ -36,7 +36,7 @@ def register():
 		existing_user = users.find_one({'username' : request.form['username']})
 		if existing_user is None:
 			hashpass = bcrypt.hashpw(request.form['password'].encode('utf-8'), bcrypt.gensalt())
-			users.insert({'username' : request.form['username'], 'password' : hashpass})
+			users.insert_one({'username' : request.form['username'], 'password' : hashpass, 'cart' : [], 'address': "", 'mobile': "", 'purchased':[]})
 			session['username'] = request.form['username']
 			return redirect(url_for('index'))
 		return 'That username already exists!'
@@ -46,7 +46,7 @@ def register():
 def logout():
 	if 'username' in session:
 		session.pop('username')
-	return render_template('index.html')
+	return redirect(url_for('index'))
 
 def redirect_url():
     return request.args.get('next') or \
@@ -57,15 +57,49 @@ def redirect_url():
 def viewProducts ():
 	#Display the Products
 	if 'username' in session:
-		return render_template("products.html",products = products.find())
-	return render_template('index.html')
+		return render_template("products.html",products = products.find(), user = users.find_one({"username" : session["username"]}))
+	return redirect(url_for('index'))
 
 @app.route("/products/<int:productId>")
 def viewIndividualProduct (productId):
-	#Display the Products
+	#Display the individual product
 	if 'username' in session:
-		return render_template('individual_product.html', product = products.find_one({"productId" : productId}))
-	return render_template('index.html')
+		return render_template('individual_product.html', product = products.find_one({"productId" : productId}), user = users.find_one({"username" : session["username"]}))
+	return redirect(url_for('index'))
+@app.route("/cart/<int:productId>")
+def addToCart (productId):
+	#add item to cart
+	if 'username' in session:
+		username = session["username"]
+		user = db.users.find_one({"username": username})
+		user['cart'].append(productId)
+		db.users.update_one({"username": username}, {"$set": {"cart": user['cart']}})
+		user = db.users.find_one({"username": username})
+		return redirect(url_for('index'))
+	return redirect(url_for('index'))
+@app.route("/remove/<int:productId>")
+def removeFromCart (productId):
+	#add item to cart
+	if 'username' in session:
+		username = session["username"]
+		user = db.users.find_one({"username": username})
+		user['cart'] = list(filter(lambda a: a != productId, user['cart']))
+		db.users.update_one({"username": username}, {"$set": {"cart": user['cart']}})
+		return redirect(url_for('viewCart'))
+	return redirect(url_for('index'))
+@app.route("/view_cart")
+def viewCart ():
+	#view cart
+	if 'username' in session:
+		username = session["username"]
+		user = db.users.find_one({"username": username})
+		cart = dict(Counter(user["cart"]))
+		product = products.find({"productId" : { "$in" : list(cart.keys())} })
+		productsMap = dict()
+		for i in product:
+			productsMap[i["productId"]] = [i["productName"] ,i["price"]]
+		return render_template("cart.html",user = user, cart = cart, products = productsMap)
+	return redirect(url_for('index'))
 if __name__ == "__main__":
 	app.secret_key = 'mysecret'
 	app.run()
